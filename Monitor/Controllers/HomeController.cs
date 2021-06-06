8d347b7e777b34e4df8fe2e44b86f09fb3b13b77
RicardoGVN <ricardo.gomez@nerdino.com>
2021-06-06 17:10:04 -0500

    connection to main page

--- Monitor/Controllers/HomeController.cs
+++ Monitor/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Monitor.Controllers
             return View();
         }
 
+        public IActionResult Monitor()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
